6f002e7e2730c4ccf57dd4d47906f07c17b416cc
Hai Long <longx3732@gmail.com>
2024-01-23 15:24:22 +0700

    EX4

---
+++ src/component/HomePage.jsx
@@ -0,0 +1,60 @@
+import React,{ useState } from 'react'
+
+export default function HomePage() {
+    const [data,setData]=useState([])
+
+
+    const [input,setInput]=useState("")
+    const changeInput=(event)=>{
+        setInput(event.target.value);
+
+    }
+
+    const submit=()=>{
+        let newData=[...data];
+        if(input!==""){
+            if(!data.includes(input)){
+                newData.push(input)
+            }
+            else{
+                alert("Giá trị đã được sử dụng")
+            }
+        }
+        
+        else{
+            alert("Nhập giá trị vào input")
+        };
+        setData(newData);
+    }
+
+    const renderContent=()=>{
+        let newArr=[];
+        for(let i=0; i<data.length;i++){
+            newArr.push(
+                <div style={{display:'flex',width:'220px',justifyContent:'space-between'}}>
+                    <h3>{data[i]}</h3>
+                    <button onClick={()=>{deleteItem(data[i])}}>delete</button>
+                </div>
+            )
+        }
+        return newArr
+    }
+
+    const deleteItem=(item)=>{
+        let newData=[...data];
+        let indexItem=newData.findIndex((it)=>{
+            return it==item
+        })
+        newData.splice(indexItem,1)
+        setData(newData)
+
+    }
+  return (
+    <div>
+        <h1>TaskList</h1>
+       <input onChange={changeInput} type="text" />
+        <button onClick={()=>{submit()}}>Submit</button>
+        {renderContent()}
+    </div>
+  )
+}
